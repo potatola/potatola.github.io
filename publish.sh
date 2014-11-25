1d9b37f740b4ee816496cd0b057638c5ad04eec4
potatola <gengyufeng0918@gmail.com>
2014-11-25 11:21:50 +0800

    bootstrap source code added

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 pelican content -s pelicanconf.py
-ghp-import output
-git push git@github.com:potatola/potatola.github.io.git gh-pages:master
+ghp-import -b master -p output
+#ghp-import -b master output
+#git push git@github.com:potatola/potatola.github.io.git gh-pages:master
